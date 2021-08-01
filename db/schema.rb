42493c3751a45c8a6864dbf6072d3914e4acbe78
ari1021 <o.pe.3.vndt.ku@gmail.com>
2021-08-01 19:04:02 +0900

    chore: update rails version

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_20_131208) do
+ActiveRecord::Schema.define(version: 2021_08_01_100134) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", null: false
